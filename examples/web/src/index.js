d79e03a2baab8f60061559c9f67efffc6341beaa
Marcin Dziewulski <hello@mobily.pl>
2018-09-04 18:52:23 +0200

    Update examples: `web`, `native`

--- examples/web/src/index.js
+++ examples/web/src/index.js
@@ -3,6 +3,15 @@
 import React from 'react'
 import { render } from 'react-dom'
 
+import withObservables from '@nozbe/with-observables'
+import LokiJSAdapter from '@nozbe/watermelondb/adapters/lokijs'
+
+import { Model } from '@nozbe/watermelondb'
+import { field, text, children, nochange } from '@nozbe/watermelondb/decorators'
+
 import Root from 'components/Root'
+
+// eslint-disable-next-line
+console.log(withObservables, LokiJSAdapter, Model, field, text, children, nochange)
 
 render(<Root />, document.getElementById('application'))
