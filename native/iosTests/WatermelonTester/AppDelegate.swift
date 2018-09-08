588e472da1ffbe08939afb443982250fdc0f615a
radex <radexpl@gmail.com>
2018-09-08 15:02:25 +0200

    Clean up WatermelonTester

---
+++ native/iosTests/WatermelonTester/AppDelegate.swift
@@ -0,0 +1,51 @@
+import UIKit
+import WatermelonDB
+
+@UIApplicationMain
+class AppDelegate: NSObject, UIApplicationDelegate {
+    let initTime = Date()
+    var window: UIWindow?
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]? = nil
+        ) -> Bool {
+        assert(NSClassFromString("XCTest") != nil, "WatermelonTester is has to be run in Test mode, not ran directly")
+
+        let jsLocation = RCTBundleURLProvider.sharedSettings()
+            .jsBundleURL(forBundleRoot: "src/index.integrationTests.native", fallbackResource: nil)
+
+        let rootView = RCTRootView(
+            bundleURL: jsLocation,
+            moduleName: "watermelonTest",
+            initialProperties: nil,
+            launchOptions: launchOptions
+            )!
+
+        let rootVC = UIViewController()
+        rootVC.view = rootView
+
+        let window = UIWindow()
+        window.rootViewController = rootVC
+
+        self.window = window
+        window.makeKeyAndVisible()
+
+        return true
+    }
+
+    // MARK: - Singleton
+
+    private struct Singleton {
+        static var shared: AppDelegate?
+    }
+
+    override init() {
+        super.init()
+        Singleton.shared = self
+    }
+
+    class var shared: AppDelegate! {
+        return Singleton.shared
+    }
+}
--- native/iosTests/WatermelonTester/Empty.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Empty.swift
-//  WatermelonTester
-//
-//  Created by Radosław Pietruszewski on 8/9/18.
-//  Copyright © 2018 Nozbe. All rights reserved.
-//
-
-import Foundation
