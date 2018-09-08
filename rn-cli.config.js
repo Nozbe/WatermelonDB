83caadc400e01e5d6bc410e190e0ef690c5b751f
radex <radexpl@gmail.com>
2018-09-08 13:30:09 +0200

    integration tests: ignore dist/, try to use src/ (doesn't work)

--- rn-cli.config.js
+++ rn-cli.config.js
@@ -1,12 +1,20 @@
 const blacklist = require('metro/src/blacklist')
+const path = require('path')
+
+const root = path.resolve(__dirname, '..', '..')
+const resolvePath = (...paths) => path.resolve(root, ...paths)
 
 const config = {
+  getProjectRoots() {
+    return [resolvePath('src')]
+  },
   getBlacklistRE() {
     // delete __tests__ from the default blacklist
     const defaultPattern = blacklist()
       .toString()
       .slice(1, -1)
-    const newPattern = defaultPattern.replace(`|.*\\/__tests__\\/.*`, '')
+    let newPattern = defaultPattern.replace(`|.*\\/__tests__\\/.*`, '')
+    newPattern += '|dist\\/.*'
 
     return RegExp(newPattern)
   },
