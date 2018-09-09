313238f0ea5a38da0114d2d732f7d2ab0b5dec4b
radex <radexpl@gmail.com>
2018-09-09 15:40:01 +0200

    Fix CI issues

--- src/observation/encodeMatcher/test.js
+++ src/observation/encodeMatcher/test.js
@@ -2,7 +2,7 @@ import Query from 'Query'
 import * as Q from 'QueryDescription'
 import Model from 'Model'
 import encodeMatcher from 'observation/encodeMatcher'
-import { matchTests } from '../__tests__/databaseTests'
+import { matchTests } from '../../__tests__/databaseTests'
 
 const mockModelClass = { table: 'tasks' }
 const mockCollection = { modelClass: mockModelClass }
