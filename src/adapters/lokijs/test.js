ca7fe9fdfb5be7bb224a6569a216579f5b144d56
radex <radexpl@gmail.com>
2018-10-08 09:26:08 +0200

    Remove redudant test

--- src/adapters/lokijs/test.js
+++ src/adapters/lokijs/test.js
@@ -1,4 +1,4 @@
-import { makeMockTask, testSchema } from '../__tests__/helpers'
+import { testSchema } from '../__tests__/helpers'
 import commonTests from '../__tests__/commonTests'
 
 import LokiJSAdapter from './index'
@@ -14,12 +14,5 @@ describe('watermelondb/adapters/lokijs', () => {
     const [name, test] = testCase
     const adapter = newAdapter()
     it(name, test(adapter, LokiJSAdapter))
-  })
-  it('does not return any Loki junk', async () => {
-    const adapter = newAdapter()
-    const record = makeMockTask({ id: 'abc', foo: 'bar' })
-
-    await adapter.batch([['CREATE', record]])
-    expect(record._raw.$loki).toBeUndefined()
   })
 })
