c1e6aaf9c110792c9e0e7accad2112fb938c5479
Andrew Durber <aidurber@gmail.com>
2018-10-29 18:15:50 +0000

    Better flow types for withDatabase HoC

--- src/DatabaseProvider/withDatabase.js
+++ src/DatabaseProvider/withDatabase.js
@@ -2,10 +2,14 @@
 import React from 'react'
 import { DatabaseConsumer } from '.'
 
+type WithDatabaseProps<T> = {
+  ...Exact<T>,
+  database: Database,
+}
 // HoC to inject the database into the props of consumers
 export default function withDatabase(
-  Component: React.ComponentType<any>,
-): React.ComponentType<any> {
+  Component: React$ComponentType<WithDatabaseProps<InputProps>>,
+): React$ComponentType<InputProps> {
   return function DatabaseComponent(props): React.ComponentType<any> {
     return <DatabaseConsumer>{database => <Component {...props} database={database} />}</DatabaseConsumer>
   }
