97ecabcce2f9b512016503c3e9ed2934eeef0538
radex <radexpl@gmail.com>
2018-11-22 09:52:15 +0100

    [sync] sketch out synchronize() function

--- src/sync/index.js
+++ src/sync/index.js
@@ -21,6 +21,8 @@ import { columnName } from '../Schema'
 
 import { prepareMarkAsSynced, prepareCreateFromRaw, prepareUpdateFromRaw } from './syncHelpers'
 
+type Timestamp = number
+
 export type SyncTableChangeSet = $Exact<{
   created: DirtyRaw[],
   updated: DirtyRaw[],
@@ -29,6 +31,28 @@ export type SyncTableChangeSet = $Exact<{
 export type SyncDatabaseChangeSet = $Exact<{ [TableName<any>]: SyncTableChangeSet }>
 
 export type SyncLocalChanges = $Exact<{ changes: SyncDatabaseChangeSet, affectedRecords: Model[] }>
+
+export type SyncPullArgs = $Exact<{ lastSyncedAt: ?Timestamp }>
+export type SyncPullResult = $Exact<{ changes: SyncDatabaseChangeSet, timestamp: Timestamp }>
+
+export type SyncPushArgs = $Exact<{ changes: SyncDatabaseChangeSet }>
+
+export type SyncArgs = $Exact<{
+  database: Database,
+  pullChanges: SyncPullArgs => Promise<SyncPullResult>,
+  pushChanges: SyncPushArgs => Promise<void>,
+}>
+
+export async function synchronize({ database, pullChanges, pushChanges }: SyncArgs): Promise<void> {
+  // TODO: Get lastSyncedAt
+  const { changes: remoteChanges, timestamp } = await pullChanges({ lastSyncedAt: null })
+  await applyRemoteChanges(database, remoteChanges)
+  // TODO: Set lastSyncedAt
+
+  const localChanges = await fetchLocalChanges(database)
+  await pushChanges({ changes: localChanges.changes })
+  await markLocalChangesAsSynced(database, localChanges)
+}
 
 // *** Applying remote changes ***
 
