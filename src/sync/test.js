5e0c0b4f9bc93bfa22c8758edf6b7e3a611150ec
radex <radexpl@gmail.com>
2019-02-13 11:10:07 +0100

    [sync] Test that sync gets aborted if DB is reset

--- src/sync/test.js
+++ src/sync/test.js
@@ -947,6 +947,49 @@ describe('synchronize', () => {
   it.skip(`only emits one collection batch change`, async () => {
     // TODO: unskip when batch change emissions are implemented
   })
+  it('aborts if database is cleared during sync', async () => {
+    const { database, projects } = makeDatabase()
+    const pushChanges = jest.fn()
+    await expectToRejectWithMessage(
+      synchronize({
+        database,
+        pullChanges: jest.fn(async () => {
+          await database.action(() => database.unsafeResetDatabase())
+          return {
+            changes: makeChangeSet({
+              mock_projects: {
+                created: [{ id: 'new_project', name: 'remote' }],
+              },
+            }),
+            timestamp: 1500,
+          }
+        }),
+        pushChanges,
+      }),
+      /aborted/,
+    )
+    await expectToRejectWithMessage(projects.find('new_project'), /not found/)
+    expect(pushChanges).not.toHaveBeenCalled()
+  })
+  it('aborts if database is cleared during sync — different case', async () => {
+    const { database, projects } = makeDatabase()
+    await expectToRejectWithMessage(
+      synchronize({
+        database,
+        pullChanges: () => ({
+          changes: makeChangeSet({
+            mock_projects: {
+              created: [{ id: 'new_project', name: 'remote' }],
+            },
+          }),
+          timestamp: 1500,
+        }),
+        pushChanges: () => database.action(() => database.unsafeResetDatabase()),
+      }),
+      /aborted/,
+    )
+    await expectToRejectWithMessage(projects.find('new_project'), /not found/)
+  })
   it('aborts if actions are not enabled', async () => {
     const { database } = mockDatabase({ actionsEnabled: false })
 
