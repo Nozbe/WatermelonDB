7e679c952cec21c0980b2d5e93ee6bb0970fa67f
jsamelak <jarek@nozbe.com>
2019-06-21 16:27:20 +0200

    helper function for deep children fetch

--- src/Model/helpers.js
+++ src/Model/helpers.js
@@ -21,3 +21,23 @@ export const createTimestampsFor = (model: Model) => {
 
   return timestamps
 }
+
+export async function fetchChildren(model: Model) {
+  var associations = model.collection.modelClass.associations
+  var childrenKeys = Object.keys(associations).filter(key => associations[key].type === 'has_many')
+  
+  const promises = childrenKeys.map(async key => {
+    var children = await model[key].fetch()
+    var promises = children.map(async child => {
+      return await fetchChildren(child)
+    })
+    var results = await Promise.all(promises)
+    results.forEach(res => children = children.concat(res))
+    return children
+  })
+
+  var results = await Promise.all(promises)
+  var allChildren = []
+  results.forEach(res => allChildren = allChildren.concat(res))
+  return allChildren
+}
