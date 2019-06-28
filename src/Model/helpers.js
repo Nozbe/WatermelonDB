ff312f17894e3e71ffb25348884583999e0789d0
jsamelak <jarek@nozbe.com>
2019-06-28 12:52:58 +0200

    use const when possible

--- src/Model/helpers.js
+++ src/Model/helpers.js
@@ -23,20 +23,20 @@ export const createTimestampsFor = (model: Model) => {
 }
 
 export async function fetchChildren(model: Model) {
-  var associations = model.collection.modelClass.associations
-  var childrenKeys = Object.keys(associations).filter(key => associations[key].type === 'has_many')
+  const associations = model.collection.modelClass.associations
+  const childrenKeys = Object.keys(associations).filter(key => associations[key].type === 'has_many')
   
   const promises = childrenKeys.map(async key => {
     var children = await model[key].fetch()
-    var promises = children.map(async child => {
+    const promises = children.map(async child => {
       return await fetchChildren(child)
     })
-    var results = await Promise.all(promises)
+    const results = await Promise.all(promises)
     results.forEach(res => children = children.concat(res))
     return children
   })
 
-  var results = await Promise.all(promises)
+  const results = await Promise.all(promises)
   var allChildren = []
   results.forEach(res => allChildren = allChildren.concat(res))
   return allChildren
