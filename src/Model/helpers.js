4037e609a7ff3d8f5ad2841e743b4de1f382066b
jsamelak <jarek@nozbe.com>
2019-07-05 15:19:14 +0200

    separate function for retrieving children queries

--- src/Model/helpers.js
+++ src/Model/helpers.js
@@ -1,9 +1,10 @@
 // @flow
 
-import {hasIn, allPromises} from '../utils/fp'
+import {allPromises, hasIn, unnest} from '../utils/fp'
 
 import * as Q from '../QueryDescription'
 import type Model from './index'
+import type Query from '../Query/index'
 
 const hasCreatedAt = hasIn('createdAt')
 export const hasUpdatedAt = hasIn('updatedAt')
@@ -23,20 +24,23 @@ export const createTimestampsFor = (model: Model) => {
   return timestamps
 }
 
-export async function fetchChildren(model: Model): Promise<Model[]> {
+function getChildrenQueries(model: Model): Query<Model>[] {
   const associationsList: any = Object.entries(model.constructor.associations)
   const hasManyAssociations = associationsList.filter(([, value]) => value.type === 'has_many')
   const childrenQueries = hasManyAssociations.map(([key, value]) => {
     const childCollection = model.collections.get(key)
     return childCollection.query(Q.where(value.foreignKey, model.id))
   })
+  return childrenQueries
+}
 
+export async function fetchChildren(model: Model): Promise<Model[]> {
   const childPromise = async query => {
     const children = await query.fetch()
-    const grandchildren = await allPromises(child => fetchChildren(child), children)
-    return Array.prototype.concat.apply([], grandchildren).concat(children)
+    const grandchildren = await allPromises(fetchChildren, children)
+    return unnest(grandchildren).concat(children)
   }
-
-  const results = await allPromises(query => childPromise(query), childrenQueries)
-  return Array.prototype.concat.apply([], results)
+  const childrenQueries = getChildrenQueries(model)
+  const results = await allPromises(childPromise, childrenQueries)
+  return unnest(results)
 }
