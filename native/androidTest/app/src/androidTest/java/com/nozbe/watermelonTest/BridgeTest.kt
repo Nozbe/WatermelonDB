a53f1cb03cda23eed1ffd10a31711c0de14467a0
radex <radexpl@gmail.com>
2019-07-26 10:51:20 +0200

    [android] update to use cavy-native-reporter

--- native/androidTest/app/src/androidTest/java/com/nozbe/watermelonTest/BridgeTest.kt
+++ native/androidTest/app/src/androidTest/java/com/nozbe/watermelonTest/BridgeTest.kt
@@ -6,6 +6,9 @@ import android.util.Log
 import org.junit.Assert
 import org.junit.Rule
 
+import com.cavynativereporter.RNCavyNativeReporterModule
+import junit.framework.Assert.assertEquals
+
 class BridgeTest {
 
     @get:Rule
@@ -13,23 +16,8 @@ class BridgeTest {
 
     @Test
     fun testBridge() {
-        synchronized(BridgeTestReporter.testFinishedNotification) {
-            BridgeTestReporter.testFinishedNotification.wait(500000)
-        }
-        try {
-            when (val result = BridgeTestReporter.result) {
-                is BridgeTestReporter.Result.Success -> {
-                    result.result.filter { it.isNotEmpty() }.forEach { Log.d("BridgeTest", it) }
-                }
-                is BridgeTestReporter.Result.Failure -> {
-                    val failureString = result.errors.asSequence().filter {
-                        it.isNotEmpty()
-                    }.joinToString(separator = "\n")
-                    Assert.fail(failureString)
-                }
-            }
-        } catch (e: UninitializedPropertyAccessException) {
-            Assert.fail("Bridge tests timed out and a report could not have been obtained. Either JS code could not be run at all or one of the asynchronous tests never returned")
-        }
+        RNCavyNativeReporterModule.waitForReport(60)
+        val errorCount = RNCavyNativeReporterModule.cavyReport.getDouble("errorCount")
+        assertEquals(0.0, errorCount, 0.0)
     }
 }
