25fa69f733f53cf3e74ecd73538162c622d65ffc
radex <radexpl@gmail.com>
2019-09-19 12:15:05 +0200

    Successfull message pass from C++ library to app

--- native/androidTest/app/src/main/java/com/nozbe/watermelonTest/MainActivity.kt
+++ native/androidTest/app/src/main/java/com/nozbe/watermelonTest/MainActivity.kt
@@ -1,7 +1,27 @@
 package com.nozbe.watermelonTest
 
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import com.facebook.react.ReactActivity
+import com.example.hellojni.HelloJni
 
 class MainActivity : ReactActivity() {
     override fun getMainComponentName(): String? = "watermelonTest"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        AlertDialog.Builder(this)
+                .setTitle("I got a message from JNI for ya!")
+                .setMessage(HelloJni().stringFromJNI())
+
+                // Specifying a listener allows you to take an action before dismissing the dialog.
+                // The dialog is automatically dismissed when a dialog button is clicked.
+                .setPositiveButton(android.R.string.yes,null)
+
+                // A null listener allows the button to dismiss the dialog and take no further action.
+                .setNegativeButton(android.R.string.no, null)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show()
+    }
 }
