a38167214af8fd48de983eb5b069b4ec9a99255a
radex <radexpl@gmail.com>
2019-10-18 15:00:36 +0200

    [fieldObserver] modernize test

--- src/observation/fieldObserver/test.js
+++ src/observation/fieldObserver/test.js
@@ -2,27 +2,18 @@ import { Subject } from 'rxjs/Subject'
 import { mockDatabase } from '../../__tests__/testModels'
 import fieldObserver from './index'
 
-const makeDatabase = () => {
-  // TODO: Change test to actually go through the DB
-  const { database } = mockDatabase()
-
-  database.adapter = {
-    batch: jest.fn(),
-  }
-
-  return database
-}
-
-const mockTask = (database, name, isCompleted, position) =>
-  database.collections.get('mock_tasks').create(mock => {
+const createTask = (tasks, name, isCompleted, position) =>
+  tasks.create(mock => {
     mock.name = name
     mock.isCompleted = isCompleted
     mock.position = position
   })
 
+const updateTask = (task, updater) => task.collection.database.action(() => task.update(updater))
+
 describe('fieldObserver', () => {
   it('observes changes correctly', async () => {
-    const database = makeDatabase()
+    const { database, tasks } = mockDatabase({ actionsEnabled: true })
 
     // start observing
     const source = new Subject()
@@ -30,33 +21,37 @@ describe('fieldObserver', () => {
     const subscription = fieldObserver(source, ['is_completed', 'position']).subscribe(observer)
 
     // start with a few matching models
-    const m1 = await mockTask(database, 'name1', false, 10)
-    const m2 = await mockTask(database, 'name2', false, 20)
+    let m1
+    let m2
+    await database.action(async () => {
+      m1 = await createTask(tasks, 'name1', false, 10)
+      m2 = await createTask(tasks, 'name2', false, 20)
+    })
     source.next([m1, m2])
     expect(observer).toHaveBeenCalledWith([m1, m2])
     expect(observer).toHaveBeenCalledTimes(1)
 
     // add matches, remove matches
-    const m3 = await mockTask(database, 'name3', false, 30)
+    const m3 = await database.action(() => createTask(tasks, 'name3', false, 30))
     source.next([m2, m3])
     expect(observer).toHaveBeenCalledWith([m2, m3])
     expect(observer).toHaveBeenCalledTimes(2)
 
     // make some irrelevant changes (no emission)
-    await m3.update(mock => {
+    await updateTask(m3, mock => {
       mock.name = 'changed name'
     })
     expect(observer).toHaveBeenCalledTimes(2)
 
     // change a relevant field
-    await m3.update(mock => {
+    await updateTask(m3, mock => {
       mock.position += 1
     })
     expect(observer).toHaveBeenCalledWith([m2, m3])
     expect(observer).toHaveBeenCalledTimes(3)
 
     // change another relevant field
-    await m2.update(mock => {
+    await updateTask(m2, mock => {
       mock.isCompleted = true
     })
 
@@ -64,17 +59,17 @@ describe('fieldObserver', () => {
     expect(observer).toHaveBeenCalledTimes(4)
 
     // change a relevant field in a previously-observed record (no emission)
-    await m1.update(mock => {
+    await updateTask(m1, mock => {
       mock.position += 1
     })
     expect(observer).toHaveBeenCalledTimes(4)
 
     // ensure record subscriptions are disposed properly
     source.complete()
-    await m2.update(mock => {
+    await updateTask(m2, mock => {
       mock.position += 1
     })
-    await m3.update(mock => {
+    await updateTask(m3, mock => {
       mock.position += 1
     })
     subscription.unsubscribe()
