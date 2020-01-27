331c51a2b421b54dbe2e2e45cf4c002bc9e80f85
Aziz Khambati <aziz.h.khambati@gmail.com>
2020-01-27 12:21:02 +0530

    Allow @action and @action() usage

--- src/decorators/nochange/index.d.ts
+++ src/decorators/nochange/index.d.ts
@@ -1,5 +1,8 @@
 declare module '@nozbe/watermelondb/decorators/nochange' {
-  const nochange: PropertyDecorator
+
+  // Copied from lib.es5.d.ts, PropertyDecorator
+  function nochange(target: Object, propertyKey: string | symbol): void;
+  function nochange(): PropertyDecorator;
 
   export default nochange
 }
--- src/decorators/readonly/index.d.ts
+++ src/decorators/readonly/index.d.ts
@@ -1,5 +1,8 @@
 declare module '@nozbe/watermelondb/decorators/readonly' {
-  const readonly: PropertyDecorator
+
+  // Copied from lib.es5.d.ts, PropertyDecorator
+  function readonly(target: Object, propertyKey: string | symbol): void;
+  function readonly(): PropertyDecorator;
 
   export default readonly
 }
