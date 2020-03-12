9b8d0aedb9770eeef297f25a84adb59658c39563
radex <radexpl@gmail.com>
2020-03-12 09:05:59 +0100

    Try to run sqlite integration tests on jsi

--- src/adapters/sqlite/integrationTest.js
+++ src/adapters/sqlite/integrationTest.js
@@ -7,42 +7,63 @@ import { invariant } from '../../utils/common'
 import DatabaseAdapterCompat from '../compat'
 
 const SQLiteAdapterTest = spec => {
-  const runTests = isSynchronous => {
+  spec.describe('SQLiteAdapter (async mode)', () => {
+    spec.it('configures adapter correctly', () => {
+      const adapter = new SQLiteAdapter({ schema: testSchema })
+      expect(adapter._dispatcherType).toBe('asynchronous')
+    })
     commonTests().forEach(testCase => {
       const [name, test] = testCase
       spec.it(name, async () => {
-        const adapter = new SQLiteAdapter({ schema: testSchema, synchronous: isSynchronous })
-
-        if (isSynchronous) {
-          if (Platform.OS === 'ios') {
-            invariant(adapter._synchronous === true, 'this should be synchronous')
-          } else {
-            invariant(
-              adapter._synchronous === false,
-              'this should be asynchronous - android does not support synchronous adaoter',
-            )
-          }
-
-          // Temporary workaround for the race condition - wait until next macrotask to ensure that
-          // database has set up
-          await new Promise(resolve => setTimeout(resolve, 0))
-        } else {
-          invariant(adapter._synchronous === false, 'this should be asynchronous')
-        }
+        const adapter = new SQLiteAdapter({ schema: testSchema, synchronous: false })
+        invariant(adapter._dispatcherType === 'asynchronous', 'this should be asynchronous')
         await test(new DatabaseAdapterCompat(adapter), SQLiteAdapter)
       })
     })
-  }
-
-  spec.describe('SQLiteAdapter (asynchronous mode)', () => {
-    spec.it('configures adapter correctly', () => {
-      const adapter = new SQLiteAdapter({ schema: testSchema })
-      expect(adapter._synchronous).toBe(false)
-    })
-    runTests(false)
   })
   spec.describe('SQLiteAdapter (synchronous mode)', () => {
-    runTests(true)
+    commonTests().forEach(testCase => {
+      const [name, test] = testCase
+      spec.it(name, async () => {
+        const adapter = new SQLiteAdapter({ schema: testSchema, synchronous: true })
+
+        if (Platform.OS === 'ios') {
+          invariant(adapter._dispatcherType === 'synchronous', 'this should be synchronous')
+        } else {
+          invariant(
+            adapter._dispatcherType === 'asynchronous',
+            'this should be asynchronous - android does not support synchronous adaoter',
+          )
+        }
+
+        // Temporary workaround for the race condition - wait until next macrotask to ensure that
+        // database has set up
+        await new Promise(resolve => setTimeout(resolve, 0))
+        await test(new DatabaseAdapterCompat(adapter), SQLiteAdapter)
+      })
+    })
+  })
+  spec.describe('SQLiteAdapter (JSI mode)', () => {
+    commonTests().forEach(testCase => {
+      const [name, test] = testCase
+      spec.it(name, async () => {
+        const adapter = new SQLiteAdapter({ schema: testSchema, experimentalUseJSI: true })
+
+        if (Platform.OS === 'ios') {
+          invariant(adapter._dispatcherType === 'jsi', 'ios shoudl support jsi')
+        } else {
+          invariant(
+            adapter._dispatcherType === 'asynchronous',
+            'this should be asynchronous - android does not support jsi adaoter yet',
+          )
+        }
+
+        // Temporary workaround for the race condition - wait until next macrotask to ensure that
+        // database has set up
+        await new Promise(resolve => setTimeout(resolve, 0))
+        await test(new DatabaseAdapterCompat(adapter), SQLiteAdapter)
+      })
+    })
   })
 }
 
