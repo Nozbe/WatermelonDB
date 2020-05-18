2b7e93456f429ecd37dc38b28892e0c95a6e51ff
radex <radexpl@gmail.com>
2020-05-18 19:15:51 +0200

    [android] compile jsi in the tester project

--- native/androidTest/app/src/main/java/com/nozbe/watermelonTest/MainActivity.kt
+++ native/androidTest/app/src/main/java/com/nozbe/watermelonTest/MainActivity.kt
@@ -1,7 +1,24 @@
 package com.nozbe.watermelonTest
 
 import com.facebook.react.ReactActivity
+import com.facebook.react.bridge.ReactContext
+import com.facebook.react.ReactInstanceManager
+import com.nozbe.watermelondb.jsi.WatermelonJSIInstaller
 
-class MainActivity : ReactActivity() {
+class MainActivity : ReactActivity(), ReactInstanceManager.ReactInstanceEventListener {
     override fun getMainComponentName(): String? = "watermelonTest"
+
+    public override fun onResume() {
+        super.onResume()
+        reactInstanceManager.addReactInstanceEventListener(this)
+    }
+
+    public override fun onPause() {
+        super.onPause()
+        reactInstanceManager.removeReactInstanceEventListener(this)
+    }
+
+    override fun onReactContextInitialized(context: ReactContext) {
+        WatermelonJSIInstaller().installBinding(context.javaScriptContextHolder.get())
+    }
 }
