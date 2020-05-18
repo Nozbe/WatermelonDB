42f747c84f4f7b222539203d695274c824aeab4f
radex <radexpl@gmail.com>
2020-05-18 14:54:50 +0200

    Add Sqlite C++ helpers

---
+++ native/ios/WatermelonDB/Sqlite.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#import <string>
+#import <sqlite3.h>
+
+namespace watermelondb {
+
+// Lightweight wrapper for handling sqlite3 lifetime
+class SqliteDb {
+public:
+    SqliteDb(std::string path);
+    ~SqliteDb();
+
+    sqlite3 *sqlite;
+
+    SqliteDb &operator=(const SqliteDb &) = delete;
+    SqliteDb(const SqliteDb &) = delete;
+};
+
+class SqliteStatement {
+public:
+    SqliteStatement(sqlite3_stmt *statement);
+    ~SqliteStatement();
+
+    sqlite3_stmt *stmt;
+
+    void reset();
+};
+
+} // namespace watermelondb
+
