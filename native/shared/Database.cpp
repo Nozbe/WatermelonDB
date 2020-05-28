b637df3bc67fa3df6d585c5d2f1ca632381b5fe5
radex <radexpl@gmail.com>
2020-05-28 10:28:39 +0200

    JSI cacheKey note

--- native/shared/Database.cpp
+++ native/shared/Database.cpp
@@ -33,7 +33,7 @@ Database::~Database() {
 }
 
 std::string cacheKey(std::string tableName, std::string recordId) {
-    return tableName + "$" + recordId; // NOTE: safe as long as neither table names nor record ids can contain $ sign
+    return tableName + "$" + recordId; // NOTE: safe as long as table names cannot contain $ sign
 }
 
 bool Database::isCached(std::string cacheKey) {
