6d3b90c76dd329a9b2984f554373ec17fb0e7454
radex <radexpl@gmail.com>
2020-12-17 11:12:59 +0100

    fix up

--- src/Model/helpers.js
+++ src/Model/helpers.js
@@ -9,7 +9,8 @@ import type Query from '../Query/index'
 const hasCreatedAt = hasIn('createdAt')
 export const hasUpdatedAt: any = hasIn('updatedAt')
 
-export const createTimestampsFor = (model: Model): {|created_at: number, updated_at: number|} => {
+type TimestampsObj = $Exact<{ created_at?: number, updated_at?: number }>
+export const createTimestampsFor = (model: Model): TimestampsObj => {
   const date = Date.now()
   const timestamps = {}
 
@@ -21,7 +22,7 @@ export const createTimestampsFor = (model: Model): {|created_at: number, updated
     timestamps.updated_at = date
   }
 
-  return timestamps
+  return (timestamps: any)
 }
 
 function getChildrenQueries(model: Model): Query<Model>[] {
