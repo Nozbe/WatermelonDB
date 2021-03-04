a30a080912332882309398954f7c93387995d7be
radex <radexpl@gmail.com>
2021-03-04 15:27:20 +0100

    [fp] Rewrite arrayDifference to remove unnecessary fp utils

--- src/utils/fp/arrayDifference/index.js
+++ src/utils/fp/arrayDifference/index.js
@@ -1,11 +1,33 @@
 // @flow
-import withoutIdentical from '../withoutIdentical'
 
 export type ArrayDiff<T> = $Exact<{ added: T[], removed: T[] }>
 
-const arrayDifference = <A, T: A>(previousList: T[], newList: T[]): ArrayDiff<T> => ({
-  added: withoutIdentical(previousList, newList),
-  removed: withoutIdentical(newList, previousList),
-})
+const arrayDifference = <A, T: A>(previousList: T[], newList: T[]): ArrayDiff<T> => {
+  // TODO: Use Set to avoid n^2 complexity
+  const added = []
+  const removed = []
+
+  outerPrevious: for (let i = 0, len = previousList.length; i < len; i++) {
+    const previousItem = previousList[i]
+    for (let j = 0, len2 = newList.length; j < len2; j++) {
+      if (previousItem === newList[j]) {
+        continue outerPrevious
+      }
+    }
+    removed.push(previousItem)
+  }
+
+  outerNew: for (let i = 0, len = newList.length; i < len; i++) {
+    const newItem = newList[i]
+    for (let j = 0, len2 = previousList.length; j < len2; j++) {
+      if (newItem === previousList[j]) {
+        continue outerNew;
+      }
+    }
+    added.push(newItem)
+  }
+
+  return { added, removed }
+}
 
 export default arrayDifference
