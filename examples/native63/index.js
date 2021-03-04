6246cb61ac9f917c0fa1beb614a17f95de1c84a4
henrymoulton <30093692+henrymoulton@users.noreply.github.com>
2021-03-04 03:22:47 +0000

    feat: PerformancePlugin ios

--- examples/native63/index.js
+++ examples/native63/index.js
@@ -2,8 +2,14 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry, NativeModules } from 'react-native'
+import App from './App'
+import { name as appName } from './app.json'
+
+const appStartedLaunchingAt = NativeModules.PerformancePlugin.appInitTimestamp
+const timeToLaunch = new Date().getTime() - appStartedLaunchingAt
+
+console.log({appStartedLaunchingAt})
+console.log({timeToLaunch})
 
 AppRegistry.registerComponent(appName, () => App);
