24933f3685e720146f7db8a95a49bd5920943e69
radex <radexpl@gmail.com>
2021-03-04 15:30:18 +0100

    Fix jest

--- jest.config.js
+++ jest.config.js
@@ -17,4 +17,5 @@ module.exports = {
   collectCoverageFrom: ['!**/node_modules/**', 'src/**'],
   coverageDirectory: 'coverage',
   coverageReporters: ['html', 'json'],
+  transformIgnorePatterns: ['node_modules/(?!(rambdax|rxjs/_esm2015)/)'],
 }
