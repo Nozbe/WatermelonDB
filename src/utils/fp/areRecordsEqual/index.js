9de182a382e4ecb8f7128ba4858ac9bb524b7d6f
radex <radexpl@gmail.com>
2021-03-18 08:59:12 +0100

    note to self

--- src/utils/fp/areRecordsEqual/index.js
+++ src/utils/fp/areRecordsEqual/index.js
@@ -1,5 +1,6 @@
 // @flow
 
+// NOTE: Only use with records! Not guaranteed to work correctly if keys have undefineds as values
 export default function areRecordsEqual<T: {}>(left: T, right: T): boolean {
   if (left === right) {
     return true
