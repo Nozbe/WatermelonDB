583f38bf1f7828087fce7e2ee9ed07d751079e4a
henrymoulton <30093692+henrymoulton@users.noreply.github.com>
2021-03-25 20:16:23 +0000

    fix: android setup must be manual

--- examples/native63/android/app/src/main/java/com/native63/MainApplication.java
+++ examples/native63/android/app/src/main/java/com/native63/MainApplication.java
@@ -10,6 +10,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.nozbe.watermelondb.WatermelonDBPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -26,6 +27,8 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+          packages.add(new WatermelonDBPackage());
+
           return packages;
         }
 
