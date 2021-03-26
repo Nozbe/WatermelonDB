bf7c8b09c244b390633cca279960904f70e3b7b9
henrymoulton <30093692+henrymoulton@users.noreply.github.com>
2021-03-26 19:40:41 +0000

    fix: adding comments

--- examples/native63/src/components/Post.js
+++ examples/native63/src/components/Post.js
@@ -8,6 +8,7 @@ import prompt from './helpers/prompt';
 import Button from './helpers/Button';
 import {extractId} from '../utils';
 
+
 const renderComment = ({item}) => <Comment comment={item} key={item.id} />;
 
 class Post extends Component {
