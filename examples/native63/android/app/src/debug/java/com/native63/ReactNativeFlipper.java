4023cb27e2f312288137cf1c530d68b1d828a57f
henrymoulton <30093692+henrymoulton@users.noreply.github.com>
2021-03-26 23:44:48 +0000

    fix: flipper working

--- examples/native63/android/app/src/debug/java/com/native63/ReactNativeFlipper.java
+++ examples/native63/android/app/src/debug/java/com/native63/ReactNativeFlipper.java
@@ -7,11 +7,14 @@
 package com.native63;
 
 import android.content.Context;
+
 import com.facebook.flipper.android.AndroidFlipperClient;
 import com.facebook.flipper.android.utils.FlipperUtils;
 import com.facebook.flipper.core.FlipperClient;
 import com.facebook.flipper.plugins.crashreporter.CrashReporterPlugin;
 import com.facebook.flipper.plugins.databases.DatabasesFlipperPlugin;
+import com.facebook.flipper.plugins.databases.impl.SqliteDatabaseDriver;
+import com.facebook.flipper.plugins.databases.impl.SqliteDatabaseProvider;
 import com.facebook.flipper.plugins.fresco.FrescoFlipperPlugin;
 import com.facebook.flipper.plugins.inspector.DescriptorMapping;
 import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin;
@@ -22,51 +25,70 @@ import com.facebook.flipper.plugins.sharedpreferences.SharedPreferencesFlipperPl
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.modules.network.NetworkingModule;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 import okhttp3.OkHttpClient;
 
 public class ReactNativeFlipper {
-  public static void initializeFlipper(Context context, ReactInstanceManager reactInstanceManager) {
-    if (FlipperUtils.shouldEnableFlipper(context)) {
-      final FlipperClient client = AndroidFlipperClient.getInstance(context);
+    public static void initializeFlipper(Context context, ReactInstanceManager reactInstanceManager) {
+        if (FlipperUtils.shouldEnableFlipper(context)) {
+            final FlipperClient client = AndroidFlipperClient.getInstance(context);
 
-      client.addPlugin(new InspectorFlipperPlugin(context, DescriptorMapping.withDefaults()));
-      client.addPlugin(new ReactFlipperPlugin());
-      client.addPlugin(new DatabasesFlipperPlugin(context));
-      client.addPlugin(new SharedPreferencesFlipperPlugin(context));
-      client.addPlugin(CrashReporterPlugin.getInstance());
+            client.addPlugin(new InspectorFlipperPlugin(context, DescriptorMapping.withDefaults()));
+            client.addPlugin(new ReactFlipperPlugin());
 
-      NetworkFlipperPlugin networkFlipperPlugin = new NetworkFlipperPlugin();
-      NetworkingModule.setCustomClientBuilder(
-          new NetworkingModule.CustomClientBuilder() {
-            @Override
-            public void apply(OkHttpClient.Builder builder) {
-              builder.addNetworkInterceptor(new FlipperOkhttpInterceptor(networkFlipperPlugin));
-            }
-          });
-      client.addPlugin(networkFlipperPlugin);
-      client.start();
+            client.addPlugin(new DatabasesFlipperPlugin(new SqliteDatabaseDriver(context, new SqliteDatabaseProvider() {
+                @Override
+                public List<File> getDatabaseFiles() {
+                    List<File> databaseFiles = new ArrayList<>();
+                    for (String databaseName : context.databaseList()) {
+                        databaseFiles.add(context.getDatabasePath(databaseName));
+                    }
+                    // https://github.com/Nozbe/WatermelonDB/issues/653#issuecomment-609734515
+                    String watermelondb = context.getDatabasePath("WatermelonDemo.db").getPath().replace("/databases", "");
+                    databaseFiles.add(new File(watermelondb));
+                    return databaseFiles;
+                }
+            })));
 
-      // Fresco Plugin needs to ensure that ImagePipelineFactory is initialized
-      // Hence we run if after all native modules have been initialized
-      ReactContext reactContext = reactInstanceManager.getCurrentReactContext();
-      if (reactContext == null) {
-        reactInstanceManager.addReactInstanceEventListener(
-            new ReactInstanceManager.ReactInstanceEventListener() {
-              @Override
-              public void onReactContextInitialized(ReactContext reactContext) {
-                reactInstanceManager.removeReactInstanceEventListener(this);
-                reactContext.runOnNativeModulesQueueThread(
-                    new Runnable() {
-                      @Override
-                      public void run() {
-                        client.addPlugin(new FrescoFlipperPlugin());
-                      }
+            client.addPlugin(new SharedPreferencesFlipperPlugin(context));
+            client.addPlugin(CrashReporterPlugin.getInstance());
+
+            NetworkFlipperPlugin networkFlipperPlugin = new NetworkFlipperPlugin();
+            NetworkingModule.setCustomClientBuilder(
+                    new NetworkingModule.CustomClientBuilder() {
+                        @Override
+                        public void apply(OkHttpClient.Builder builder) {
+                            builder.addNetworkInterceptor(new FlipperOkhttpInterceptor(networkFlipperPlugin));
+                        }
                     });
-              }
-            });
-      } else {
-        client.addPlugin(new FrescoFlipperPlugin());
-      }
+            client.addPlugin(networkFlipperPlugin);
+            client.start();
+
+            // Fresco Plugin needs to ensure that ImagePipelineFactory is initialized
+            // Hence we run if after all native modules have been initialized
+            ReactContext reactContext = reactInstanceManager.getCurrentReactContext();
+            if (reactContext == null) {
+                reactInstanceManager.addReactInstanceEventListener(
+                        new ReactInstanceManager.ReactInstanceEventListener() {
+                            @Override
+                            public void onReactContextInitialized(ReactContext reactContext) {
+                                reactInstanceManager.removeReactInstanceEventListener(this);
+                                reactContext.runOnNativeModulesQueueThread(
+                                        new Runnable() {
+                                            @Override
+                                            public void run() {
+                                                client.addPlugin(new FrescoFlipperPlugin());
+                                            }
+                                        });
+                            }
+                        });
+            } else {
+                client.addPlugin(new FrescoFlipperPlugin());
+            }
+        }
     }
-  }
 }
