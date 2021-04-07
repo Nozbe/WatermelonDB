029106cbea4e450a4fd535a862a893ec8ab47466
radex <radexpl@gmail.com>
2021-04-07 15:42:37 +0200

    Alternative proguard fix that does not require androidx

--- native/android-jsi/src/main/java/com/nozbe/watermelondb/jsi/JSIInstaller.java
+++ native/android-jsi/src/main/java/com/nozbe/watermelondb/jsi/JSIInstaller.java
@@ -1,16 +1,18 @@
 package com.nozbe.watermelondb.jsi;
 
 import android.content.Context;
-import androidx.annotation;
-
 class JSIInstaller {
     static void install(Context context, long javaScriptContextHolder) {
         JSIInstaller.context = context;
         new JSIInstaller().installBinding(javaScriptContextHolder);
+
+        // call methods we're going to need from JNI - if we don't, Proguard/R8 will strip it from
+        // release binaries. We could use @Keep or configure Proguard to keep it but that would be
+        // error prone for lib users
+        _resolveDatabasePath("");
     }
 
     // Helper method called from C++
-    @Keep
     static String _resolveDatabasePath(String dbName) {
         // On some systems there is some kind of lock on `/databases` folder ¯\_(ツ)_/¯
         return context.getDatabasePath(dbName + ".db").getPath().replace("/databases", "");
