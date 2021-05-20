f4b9d78268862ab9f4efd2571da29e27554b4758
radex <radexpl@gmail.com>
2021-05-20 14:53:29 +0200

    pods: Remove dependency on Nimble

--- native/iosTest/Pods/Nimble/Sources/Nimble/Nimble.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-#import "NMBExceptionCapture.h"
-#import "NMBStringify.h"
-#import "DSL.h"
-
-#import "CwlPreconditionTesting.h"
-
-FOUNDATION_EXPORT double NimbleVersionNumber;
-FOUNDATION_EXPORT const unsigned char NimbleVersionString[];
