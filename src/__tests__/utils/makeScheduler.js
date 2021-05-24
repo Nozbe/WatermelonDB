aab339253459b253d97eb983aac4390fb552181c
radex <radexpl@gmail.com>
2021-05-24 12:05:35 +0200

    Clean up databaseTests

--- src/__tests__/utils/makeScheduler.js
+++ src/__tests__/utils/makeScheduler.js
@@ -1,23 +1,17 @@
-// @flow
-
-// $FlowFixMe
 // eslint-disable-next-line
 import { TestScheduler } from 'rxjs/internal/testing/TestScheduler'
-import type { /* ColdObservable, HotObservable */ Observable } from 'rxjs'
-
-type HotObservable<+T> = Observable<T>
 
 class WatermelonTestScheduler extends TestScheduler {
-  cold<T>(marbles: string, values?: any, error?: any): ColdObservable<T> {
+  cold(marbles, values, error) {
     return this.createColdObservable(marbles, values, error)
   }
 
-  hot<T>(marbles: string, values?: any, error?: any): HotObservable<T> {
+  hot(marbles, values, error) {
     return this.createHotObservable(marbles, values, error)
   }
 }
 
-export default function makeScheduler(): WatermelonTestScheduler {
+export default function makeScheduler() {
   return new WatermelonTestScheduler((actual, expected) => {
     expect(actual).toEqual(expected)
   })
