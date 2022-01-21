c12d56151fe418c9aaf1212af163148702d9368d
Kudo Chien <ckchien@gmail.com>
2022-01-21 20:51:17 +0800

    Fix building error in expo project

--- native/ios/WatermelonDB/SupportingFiles/Bridging.h
+++ native/ios/WatermelonDB/SupportingFiles/Bridging.h
@@ -6,12 +6,6 @@
 
 #import <React/RCTBridgeModule.h>
 
-#if __has_include("JSIInstaller.h")
-#import "JSIInstaller.h"
-#else
-#import "../JSIInstaller.h"
-#endif
-
 #if __has_include("DatabaseDeleteHelper.h")
 #import "DatabaseDeleteHelper.h"
 #else
