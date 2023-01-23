5d348f2efc733ab7e81c10c68428f937ff34c7c3
Paul Rostorp <paul.rostorp@gmail.com>
2023-01-23 13:30:27 +0100

    fix `DatabaseProvider` & `withDatabase` HOC

--- src/DatabaseProvider/withDatabase.d.ts
+++ src/DatabaseProvider/withDatabase.d.ts
@@ -1,11 +1,11 @@
+import type { NonReactStatics } from 'hoist-non-react-statics'
 import { ComponentType } from 'react'
 import type Database from '../Database'
-import { DatabaseConsumer } from './DatabaseContext'
 
 type WithDatabaseProps<T> = T & {
   database: Database
 }
 // HoC to inject the database into the props of consumers
-export default function withDatabase<T>(
+export default function withDatabase<T extends React.ComponentType<any>>(
   Component: ComponentType<WithDatabaseProps<T>>,
-): DatabaseConsumer
+): React.FunctionComponent<Omit<T, 'database'>> & NonReactStatics<T>
