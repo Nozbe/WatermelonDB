7ce8e5744469b52398ee1489045e43547537cf32
Paul Rostorp <paul.rostorp@gmail.com>
2023-01-24 13:31:01 +0100

    extend typescript example

---
+++ examples/typescript/AppSchema.ts
@@ -0,0 +1,22 @@
+import { appSchema, tableSchema } from "@nozbe/watermelondb"
+// eslint-disable-next-line import/no-unresolved, import/extensions
+import { TableName } from "./ts-example"
+
+export const AppSchema = appSchema({
+  version: 1,
+  tables: [
+    tableSchema({
+      name: TableName.BLOGS,
+      columns: [{ name: 'name', type: 'string' }],
+    }),
+    tableSchema({
+      name: TableName.POSTS,
+      columns: [
+        { name: 'title', type: 'string' },
+        { name: 'body', type: 'string' },
+        { name: 'blog_id', type: 'string', isIndexed: true },
+        { name: 'is_nasty', type: 'boolean' },
+      ],
+    }),
+  ],
+})
--- examples/typescript/ts-example.ts
+++ examples/typescript/ts-example.ts
@@ -1,13 +1,17 @@
 // tslint:disable: max-classes-per-file
-import { Model, Q, Query, Relation } from '@nozbe/watermelondb'
-import { action, children, field, lazy, relation } from '@nozbe/watermelondb/decorators'
-import { Associations } from '@nozbe/watermelondb/Model'
+import { Database, Model, Q, Query, Relation } from '@nozbe/watermelondb'
+import { action, children, field, lazy, relation, text } from '@nozbe/watermelondb/decorators'
+import { addColumns, schemaMigrations } from '@nozbe/watermelondb/Schema/migrations'
 import { setGenerator } from '@nozbe/watermelondb/utils/common/randomId'
-
+import SQLiteAdapter from "@nozbe/watermelondb/adapters/sqlite"
+import { Associations } from '@nozbe/watermelondb/Model'
+import { SyncDatabaseChangeSet, synchronize } from "@nozbe/watermelondb/sync"
+// eslint-disable-next-line import/no-unresolved, import/extensions
+import { AppSchema } from "./AppSchema"
 // Create an enum for all Table Names.
 // This will help in documenting where all exact table names need to be passed.
 // eslint-disable-next-line no-shadow
-enum TableName {
+export enum TableName {
   BLOGS = 'blogs',
   POSTS = 'posts',
 }
@@ -40,6 +44,7 @@ class Post extends Model {
   }
 
   @field('name') name!: string;
+  @text("body") content!: string;
   @field('is_nasty') isNasty!: boolean;
 
   @relation(TableName.BLOGS, 'blog_id') blog!: Relation<Blog>;
@@ -54,3 +59,49 @@ setGenerator(randomString)
 
 // or as anonymous function:
 setGenerator(() => 'RANDOM STRING')
+
+const adapter = new SQLiteAdapter({
+  schema: AppSchema,
+  migrations: schemaMigrations({
+    migrations: [
+      {
+        toVersion: 1,
+        steps: [
+          addColumns({
+            table: TableName.POSTS,
+            columns: [{ name: "body", type: "string", isIndexed: true, isOptional: false }],
+          }),
+        ],
+      },
+    ],
+  }),
+  onSetUpError: (error): void => { },
+})
+
+const db = new Database({
+  adapter,
+  modelClasses: [Blog, Post],
+
+})
+
+const sync = async () => {
+  return synchronize({
+    database: db,
+    async pullChanges({ lastPulledAt, schemaVersion, migration }) {
+      // just for demo purposes, this should come from the server
+      const serverTS = new Date().getTime()
+      const serverChanges: SyncDatabaseChangeSet = {
+        posts: {
+          created: [],
+          updated: [],
+          deleted: ["some-id"],
+        },
+      }
+
+      return { changes: serverChanges, timestamp: serverTS }
+    },
+    async pushChanges({ changes, lastPulledAt }) {
+      return undefined
+    },
+  })
+}
