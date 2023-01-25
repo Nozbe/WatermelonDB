fbd43592f6233e832dafcd364a56695e800f976c
radex <radexpl@gmail.com>
2023-01-26 00:23:15 +0100

    fix flow

--- src/__tests__/utils/expectToRejectWithMessage/index.js
+++ src/__tests__/utils/expectToRejectWithMessage/index.js
@@ -4,7 +4,9 @@ export default async function expectToRejectWithMessage(
   promise: Promise<mixed>,
   message: string | RegExp,
 ): Promise<void> {
+  // $FlowFixMe
   await expect(promise).rejects.toMatchObject({
+    // $FlowFixMe
     message: expect.stringMatching(message),
   })
 }
