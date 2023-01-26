99a4cc7b99e290b55a4e859e40ce06551940edc8
radex <radexpl@gmail.com>
2023-01-26 21:12:19 +0100

    Fix Cocoapods

--- native/iosTest/Pods/Headers/Private/React-Codegen/FBReactNativeSpecJSI.h
+++
@@ -1,1 +0,0 @@
-../../../../build/generated/ios/FBReactNativeSpecJSI.h
--- native/iosTest/Pods/Target Support Files/React-Codegen/React-Codegen-umbrella.h
+++ native/iosTest/Pods/Target Support Files/React-Codegen/React-Codegen-umbrella.h
@@ -11,7 +11,6 @@
 #endif
 
 #import "FBReactNativeSpec/FBReactNativeSpec.h"
-#import "FBReactNativeSpecJSI.h"
 
 FOUNDATION_EXPORT double React_CodegenVersionNumber;
 FOUNDATION_EXPORT const unsigned char React_CodegenVersionString[];
