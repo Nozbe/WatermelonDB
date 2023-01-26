745b617491c01365c21788e5509f9fa08878e3c8
radex <radexpl@gmail.com>
2023-01-26 15:39:38 +0100

    Add WatermelonTesterFrameworks target

---
+++ native/iosTest/WatermelonTesterFrameworks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WatermelonTesterFrameworks
+//
+//  Created by Radosław Pietruszewski on 26/01/2023.
+//  Copyright © 2023 Nozbe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
