7b42de88e0a68cea158eccfb374c191facf4d276
radex <radexpl@gmail.com>
2023-01-27 15:03:40 +0100

    Remove unnecessary imports from the Bridging header

--- native/iosTest/WatermelonTester/WatermelonTester-Bridging-Header.h
+++ native/iosTest/WatermelonTester/WatermelonTester-Bridging-Header.h
@@ -1,7 +1,3 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 #import <React/RCTViewManager.h>
-#import <React/RCTBridgeModule.h>
-
-// Silence warning
-//#import "../../ios/WatermelonDB/SupportingFiles/Bridging.h"
