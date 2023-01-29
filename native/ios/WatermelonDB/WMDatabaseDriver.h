29d5724753537fd20b36ab2d0261c765638cfd8f
radex <radexpl@gmail.com>
2023-01-29 22:32:34 +0100

    + WMDatabaseDriver querying and cache options

--- native/ios/WatermelonDB/WMDatabaseDriver.h
+++ native/ios/WatermelonDB/WMDatabaseDriver.h
@@ -5,6 +5,7 @@ NS_ASSUME_NONNULL_BEGIN
 @interface WMDatabaseDriver : NSObject
 
 @property (readwrite, strong, nonatomic) WMDatabase *db;
+@property (readonly, strong, nonatomic) NSMutableDictionary<NSString *, NSMutableSet<NSString *> *> *cachedRecords;
 
 @end
 
--- native/ios/WatermelonDB/WMDatabaseDriver.m
+++ native/ios/WatermelonDB/WMDatabaseDriver.m
@@ -8,12 +8,13 @@ typedef NS_ENUM(NSInteger, WMDatabaseCompatibility) {
 
 @implementation WMDatabaseDriver
 
-#pragma - Initialization
+#pragma mark - Initialization
 
 - (instancetype) initWithName:(NSString *)dbName
 {
     if (self = [super init]) {
         _db = [WMDatabase databaseWithPath:[self pathForName:dbName]];
+        _cachedRecords = [NSMutableDictionary dictionary];
     }
     
     return self;
@@ -40,7 +41,7 @@ typedef NS_ENUM(NSInteger, WMDatabaseCompatibility) {
     }
 }
 
-#pragma - Setup
+#pragma mark - Setup
 
 - (long) schemaVersion
 {
@@ -62,6 +63,109 @@ typedef NS_ENUM(NSInteger, WMDatabaseCompatibility) {
         NSLog(@"Database has newer version (%li) than what the app supports (%li). Will reset database.", dbVersion, version);
         return WMDatabaseCompatibilityNeedsSetup;
     }
+}
+
+// TODO: setUpWithSchema
+// TODO: setUpWithMigrations
+
+#pragma mark - Database functions
+
+- (id) find:(NSString *)table id:(NSString *)id error:(NSError **)errorPtr
+{
+    if ([self isCached:table id:id]) {
+        return id;
+    }
+    
+    NSString *query = [NSString stringWithFormat:@"select * from `%@` where id == ? limit 1", table];
+    FMResultSet *result = [_db queryRaw:query args:@[id] error:errorPtr];
+    
+    if (![result next]) {
+        return nil;
+    }
+    
+    [self markAsCached:table id:id];
+    return [result resultDictionary];
+}
+
+- (NSArray *) cachedQuery:(NSString *)table query:(NSString *)query args:(NSArray *)args error:(NSError **)errorPtr
+{
+    NSMutableArray *resultArray;
+    FMResultSet *result = [_db queryRaw:query args:args error:errorPtr];
+    if (!resultArray) {
+        return nil;
+    }
+    
+    while ([result next]) {
+        NSString *id = [result stringForColumn:@"id"];
+        if ([self isCached:table id:id]) {
+            [resultArray addObject:id];
+        } else {
+            [self markAsCached:table id:id];
+            [resultArray addObject:[result resultDictionary]];
+        }
+    }
+    
+    return resultArray;
+}
+
+- (NSArray *) queryIds:(NSString *)query args:(NSArray *)args error:(NSError **)errorPtr
+{
+    NSMutableArray *resultArray;
+    FMResultSet *result = [_db queryRaw:query args:args error:errorPtr];
+    if (!resultArray) {
+        return nil;
+    }
+    
+    while ([result next]) {
+        [resultArray addObject:[result stringForColumn:@"id"]];
+    }
+    
+    return resultArray;
+}
+
+- (NSArray *) unsafeQueryRaw:(NSString *)query args:(NSArray *)args error:(NSError **)errorPtr
+{
+    NSMutableArray *resultArray;
+    FMResultSet *result = [_db queryRaw:query args:args error:errorPtr];
+    if (!resultArray) {
+        return nil;
+    }
+    
+    while ([result next]) {
+        [resultArray addObject:[result resultDictionary]];
+    }
+    
+    return resultArray;
+}
+
+- (NSNumber *) count:(NSString *)query args:(NSArray *)args error:(NSError **)errorPtr
+{
+    return [_db count:query args:args error:errorPtr];
+}
+
+#pragma mark - Record caching
+
+- (BOOL) isCached:(NSString *)table id:(NSString *)id
+{
+    if ([_cachedRecords[table] containsObject:id]) {
+        return YES;
+    }
+    return NO;
+}
+
+- (void) markAsCached:(NSString *)table id:(NSString *)id
+{
+    NSMutableSet *set = _cachedRecords[table];
+    if (!set) {
+        set = [NSMutableSet set];
+        _cachedRecords[table] = set;
+    }
+    [set addObject:id];
+}
+
+- (void) removeFromCache:(NSString *)table id:(NSString *)id
+{
+    [_cachedRecords[table] removeObject:id];
 }
 
 @end
