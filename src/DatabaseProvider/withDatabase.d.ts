be5b59a960054b086dffe494454a647a4fa05d85
Paul Rostorp <paul.rostorp@gmail.com>
2023-01-30 15:37:28 +0100

    revert `withDatabase` HOC

--- src/DatabaseProvider/withDatabase.d.ts
+++ src/DatabaseProvider/withDatabase.d.ts
@@ -1,11 +1,16 @@
-import type { NonReactStatics } from 'hoist-non-react-statics'
-import { ComponentType } from 'react'
+import * as React from 'react'
+import { NonReactStatics } from 'hoist-non-react-statics'
 import type Database from '../Database'
 
 type WithDatabaseProps<T> = T & {
   database: Database
 }
+
+type GetProps<C> = C extends React.ComponentType<infer P & { database?: Database }> ? P : never
+
 // HoC to inject the database into the props of consumers
-export default function withDatabase<T extends React.ComponentType<any>>(
-  Component: ComponentType<WithDatabaseProps<T>>,
-): React.FunctionComponent<Omit<T, 'database'>> & NonReactStatics<T>
+export default function withDatabase<
+C extends React.ComponentType<any>,
+P = GetProps<C>,
+R = Omit<P, 'database'>
+>(Component: C): React.FunctionComponent<R> & NonReactStatics<C>
