546db97cf24b57b2607cc487d0af346e77bdf0cf
radex <radexpl@gmail.com>
2023-02-05 13:35:32 +0100

    clean up tests

--- src/QueryDescription/experimentalOptimize/test.js
+++ src/QueryDescription/experimentalOptimize/test.js
@@ -35,186 +35,192 @@ describe('optimizeQueryDescription', () => {
   it(`empty query`, () => {
     expect(optimize([])).toEqual([])
   })
-  it(`does not reorder conditions if profitability is unknown`, () => {
-    const orig = [
-      Q.where('foo', 'bar'),
-      Q.unsafeSqlExpr(''),
-      Q.unsafeLokiExpr({}),
-      Q.or(Q.where('foo', 'bar')),
-    ]
-    expect(optimize(orig)).toEqual(orig)
-  })
-  it(`reorders indexed columns before unindexed`, () => {
-    expect(
-      optimize([
+  describe('reorders conditions', () => {
+    it(`does not reorder conditions if profitability is unknown`, () => {
+      const orig = [
+        Q.where('foo', 'bar'),
+        Q.unsafeSqlExpr(''),
+        Q.unsafeLokiExpr({}),
+        Q.or(Q.where('foo', 'bar')),
+      ]
+      expect(optimize(orig)).toEqual(orig)
+    })
+    it(`reorders indexed columns before unindexed`, () => {
+      expect(
+        optimize([
+          //
+          Q.where('str', 'bar'),
+          Q.where('bool_i', 'bar'),
+          Q.where('str_i', 'bar'),
+        ]),
+      ).toEqual([
         //
-        Q.where('str', 'bar'),
         Q.where('bool_i', 'bar'),
         Q.where('str_i', 'bar'),
-      ]),
-    ).toEqual([
-      //
-      Q.where('bool_i', 'bar'),
-      Q.where('str_i', 'bar'),
-      Q.where('str', 'bar'),
-    ])
-  })
-  it(`reorders Q.eq before other comparisons`, () => {
-    expect(
-      optimize([
+        Q.where('str', 'bar'),
+      ])
+    })
+    it(`reorders Q.eq before other comparisons`, () => {
+      expect(
+        optimize([
+          //
+          Q.where('str', Q.gt('bar')),
+          Q.where('str', Q.notEq('bar')),
+          Q.where('str', 'bar'),
+        ]),
+      ).toEqual([
         //
+        Q.where('str', 'bar'),
         Q.where('str', Q.gt('bar')),
         Q.where('str', Q.notEq('bar')),
-        Q.where('str', 'bar'),
-      ]),
-    ).toEqual([
-      //
-      Q.where('str', 'bar'),
-      Q.where('str', Q.gt('bar')),
-      Q.where('str', Q.notEq('bar')),
-    ])
-  })
-  it(`reorders Q.oneOf depending on number of args`, () => {
-    expect(
-      optimize([
+      ])
+    })
+    it(`reorders Q.oneOf depending on number of args`, () => {
+      expect(
+        optimize([
+          //
+          Q.where('str', Q.oneOf(Array(10).fill('bar'))),
+          Q.where('str', Q.oneOf(Array(2).fill('bar'))),
+          Q.where('str', Q.oneOf(Array(5).fill('bar'))),
+          Q.where('str', 'bar'),
+        ]),
+      ).toEqual([
         //
-        Q.where('str', Q.oneOf(Array(10).fill('bar'))),
+        Q.where('str', 'bar'),
         Q.where('str', Q.oneOf(Array(2).fill('bar'))),
         Q.where('str', Q.oneOf(Array(5).fill('bar'))),
-        Q.where('str', 'bar'),
-      ]),
-    ).toEqual([
-      //
-      Q.where('str', 'bar'),
-      Q.where('str', Q.oneOf(Array(2).fill('bar'))),
-      Q.where('str', Q.oneOf(Array(5).fill('bar'))),
-      Q.where('str', Q.oneOf(Array(10).fill('bar'))),
-    ])
-  })
-  it(`flattens Q.and`, () => {
-    expect(
-      optimize([
-        //
-        Q.where('str', 'bar'),
-        Q.and([
+        Q.where('str', Q.oneOf(Array(10).fill('bar'))),
+      ])
+    })
+    it(`reorders Q.ons last`, () => {
+      expect(
+        optimize([
           //
-          Q.where('str', 'bar2'),
-          Q.and(Q.where('str', 'bar3')),
+          Q.on('comments', 'foo', 'bar'),
+          Q.where('bar', 'baz'),
         ]),
-      ]),
-    ).toEqual([
-      //
-      Q.where('str', 'bar'),
-      Q.where('str', 'bar2'),
-      Q.where('str', 'bar3'),
-    ])
-  })
-  it(`flattens Q.or`, () => {
-    expect(
-      optimize([
+      ).toEqual([
         //
-        Q.where('str', 'bar'),
-        Q.or([
-          //
-          Q.where('str', 'bar2'),
-          Q.or(Q.where('str', 'bar3'), Q.where('str', 'bar4')),
-        ]),
-      ]),
-    ).toEqual([
-      //
-      Q.where('str', 'bar'),
-      Q.or([Q.where('str', 'bar2'), Q.where('str', 'bar3'), Q.where('str', 'bar4')]),
-    ])
-  })
-  it(`flattens (merges) Q.ons`, () => {
-    expect(
-      optimize([
+        Q.where('bar', 'baz'),
         Q.on('comments', 'foo', 'bar'),
+      ])
+    })
+  })
+  describe('flattens inner lists', () => {
+    it(`flattens Q.and`, () => {
+      expect(
+        optimize([
+          //
+          Q.where('str', 'bar'),
+          Q.and([
+            //
+            Q.where('str', 'bar2'),
+            Q.and(Q.where('str', 'bar3')),
+          ]),
+        ]),
+      ).toEqual([
+        //
+        Q.where('str', 'bar'),
+        Q.where('str', 'bar2'),
+        Q.where('str', 'bar3'),
+      ])
+    })
+    it(`flattens Q.or`, () => {
+      expect(
+        optimize([
+          //
+          Q.where('str', 'bar'),
+          Q.or([
+            //
+            Q.where('str', 'bar2'),
+            Q.or(Q.where('str', 'bar3'), Q.where('str', 'bar4')),
+          ]),
+        ]),
+      ).toEqual([
+        //
+        Q.where('str', 'bar'),
+        Q.or([Q.where('str', 'bar2'), Q.where('str', 'bar3'), Q.where('str', 'bar4')]),
+      ])
+    })
+    it(`flattens (merges) Q.ons`, () => {
+      expect(
+        optimize([
+          Q.on('comments', 'foo', 'bar'),
+          Q.on('comments', [
+            //
+            Q.where('bar', 'baz'),
+            Q.where('baz', 'blah'),
+          ]),
+        ]),
+      ).toEqual([
         Q.on('comments', [
           //
+          Q.where('foo', 'bar'),
           Q.where('bar', 'baz'),
           Q.where('baz', 'blah'),
         ]),
-      ]),
-    ).toEqual([
-      Q.on('comments', [
-        //
-        Q.where('foo', 'bar'),
-        Q.where('bar', 'baz'),
-        Q.where('baz', 'blah'),
-      ]),
-    ])
+      ])
+    })
   })
-  it(`reorders Q.ons last`, () => {
-    expect(
-      optimize([
-        //
-        Q.on('comments', 'foo', 'bar'),
-        Q.where('bar', 'baz'),
-      ]),
-    ).toEqual([
-      //
-      Q.where('bar', 'baz'),
-      Q.on('comments', 'foo', 'bar'),
-    ])
-  })
-  it(`optimizes Q.and`, () => {
-    expect(
-      optimize([
+  describe('optimizes inner lists', () => {
+    it(`optimizes Q.and`, () => {
+      expect(
+        optimize([
+          Q.or(
+            Q.and(
+              //
+              Q.where('str', 'bar'),
+              Q.where('bool_i', 'bar'),
+              Q.where('str_i', 'bar'),
+            ),
+          ),
+        ]),
+      ).toEqual([
         Q.or(
           Q.and(
+            //
+            Q.where('bool_i', 'bar'),
+            Q.where('str_i', 'bar'),
+            Q.where('str', 'bar'),
+          ),
+        ),
+      ])
+    })
+    it(`optimizes Q.or`, () => {
+      expect(
+        optimize([
+          Q.or(
             //
             Q.where('str', 'bar'),
             Q.where('bool_i', 'bar'),
             Q.where('str_i', 'bar'),
           ),
-        ),
-      ]),
-    ).toEqual([
-      Q.or(
-        Q.and(
-          //
-          Q.where('bool_i', 'bar'),
-          Q.where('str_i', 'bar'),
-          Q.where('str', 'bar'),
-        ),
-      ),
-    ])
-  })
-  it(`optimizes Q.or`, () => {
-    expect(
-      optimize([
+        ]),
+      ).toEqual([
         Q.or(
           //
-          Q.where('str', 'bar'),
           Q.where('bool_i', 'bar'),
           Q.where('str_i', 'bar'),
+          Q.where('str', 'bar'),
         ),
-      ]),
-    ).toEqual([
-      Q.or(
-        //
-        Q.where('bool_i', 'bar'),
-        Q.where('str_i', 'bar'),
-        Q.where('str', 'bar'),
-      ),
-    ])
-  })
-  it(`optimizes Q.on`, () => {
-    expect(
-      optimize([
-        //
-        Q.on('comments', Q.where('str', 'bar')),
-        Q.on('comments', [Q.where('bool_i', 'bar'), Q.where('str_i', 'bar')]),
-      ]),
-    ).toEqual([
-      Q.on('comments', [
-        //
-        Q.where('bool_i', 'bar'),
-        Q.where('str_i', 'bar'),
-        Q.where('str', 'bar'),
-      ]),
-    ])
+      ])
+    })
+    it(`optimizes Q.on`, () => {
+      expect(
+        optimize([
+          //
+          Q.on('comments', Q.where('str', 'bar')),
+          Q.on('comments', [Q.where('bool_i', 'bar'), Q.where('str_i', 'bar')]),
+        ]),
+      ).toEqual([
+        Q.on('comments', [
+          //
+          Q.where('bool_i', 'bar'),
+          Q.where('str_i', 'bar'),
+          Q.where('str', 'bar'),
+        ]),
+      ])
+    })
   })
   it('deep freezes the query in dev', () => {
     const make = () => optimize([Q.where('left_column', 'right_value')])
