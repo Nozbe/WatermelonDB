072dc19aa62f0d9842e68e7a4f4124d4eb7a58e5
radex <radexpl@gmail.com>
2023-02-15 20:34:18 +0100

    fix WMDatabase.java

--- native/android/src/main/java/com/nozbe/watermelondb/WMDatabase.java
+++ native/android/src/main/java/com/nozbe/watermelondb/WMDatabase.java
@@ -8,6 +8,7 @@ import android.database.sqlite.SQLiteDatabase;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 
 public class WMDatabase {
