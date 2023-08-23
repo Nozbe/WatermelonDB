a0d0c55230c9d020db16bffe83ee9a4978a99403
radex <radexpl@gmail.com>
2023-08-23 12:40:57 +0200

    Windows: native/windowsTest stub

---
+++ native/windowsTest/WatermelonTester/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::WatermelonTester::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::WatermelonTester::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
