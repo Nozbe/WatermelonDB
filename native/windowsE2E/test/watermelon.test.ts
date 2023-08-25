0f067da70e924d13ab987b7d609699c049906ebd
radex <radexpl@gmail.com>
2023-08-25 12:36:09 +0200

    Windows: Clean up E2E runner project

--- native/windowsE2E/test/watermelon.test.ts
+++ native/windowsE2E/test/watermelon.test.ts
@@ -1,21 +1,8 @@
 import {app} from '@react-native-windows/automation';
-import {dumpVisualTree} from '@react-native-windows/automation-commands';
-
-console.log('hello')
 
 describe('WatermelonDB', () => {
-console.log('hello2')
-test('Text', async () => {
-    console.log('1')
+  test('Check integration test status', async () => {
     const view = await app.findElementByTestID('WatermelonTesterStatus');
-    console.log('2')
     console.log(await view.getText())
-    console.log('3')
-    
-    // await app.waitUntil(async () => {
-    //   const headerText = await view.getText();
-    //   return headerText.includes('Welcome');
-    // }) 
-    
   });
 })
