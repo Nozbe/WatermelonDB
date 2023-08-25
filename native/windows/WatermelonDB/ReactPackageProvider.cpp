88c396c89edea8c285f830d4db4e2a5e8a0c10f9
radex <radexpl@gmail.com>
2023-08-25 10:09:17 +0200

    Windows: Remove unnecessary module

--- native/windows/WatermelonDB/ReactPackageProvider.cpp
+++ native/windows/WatermelonDB/ReactPackageProvider.cpp
@@ -4,8 +4,6 @@
 #include "ReactPackageProvider.g.cpp"
 #endif
 
-#include "ReactNativeModule.h"
-
 // NOTE: You must include the headers of your native modules here in
 // order for the AddAttributedModules call below to find them.
 #include "WMDatabaseBridge.h"
