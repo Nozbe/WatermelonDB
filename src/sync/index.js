26f659adda4dd349e602927637340e7d76757953
Jonathan DiCamillo <jonathan@buildops.com>
2024-04-30 10:54:22 -0700

    Revert "feat: push changes before pulling"
    This reverts commit 77909c1953c454e2d16b6ba17781b7893f650042.

--- src/sync/index.js
+++ src/sync/index.js
@@ -25,7 +25,7 @@ export type SyncTableChangeSet = $Exact<{
   updated: DirtyRaw[],
   deleted: RecordId[],
 }>
-export type SyncDatabaseChangeSet = $Exact<{ [TableName < any >]: SyncTableChangeSet }>
+export type SyncDatabaseChangeSet = $Exact<{ [TableName<any>]: SyncTableChangeSet }>
 
 export type SyncLocalChanges = $Exact<{ changes: SyncDatabaseChangeSet, affectedRecords: Model[] }>
 
@@ -58,60 +58,43 @@ export type SyncConflictResolver = (
 
 export type SyncArgs = $Exact<{
   database: Database,
-  pullChanges: SyncPullArgs => Promise < SyncPullResult >,
-    pushChanges: SyncPushArgs => Promise < void>,
-      // version at which support for migration syncs was added - the version BEFORE first syncable migration
-      migrationsEnabledAtVersion ?: SchemaVersion,
-      sendCreatedAsUpdated ?: boolean,
-      log ?: SyncLog,
-      // Advanced (unsafe) customization point. Useful when you have subtle invariants between multiple
-      // columns and want to have them updated consistently, or to implement partial sync
-      // It's called for every record being updated locally, so be sure that this function is FAST.
-      // If you don't want to change default behavior for a given record, return `resolved` as is
-      // Note that it's safe to mutate `resolved` object, so you can skip copying it for performance.
-      conflictResolver ?: SyncConflictResolver,
-      // commits changes in multiple batches, and not one - temporary workaround for memory issue
-      _unsafeBatchPerCollection ?: boolean,
+  pullChanges: SyncPullArgs => Promise<SyncPullResult>,
+  pushChanges: SyncPushArgs => Promise<void>,
+  // version at which support for migration syncs was added - the version BEFORE first syncable migration
+  migrationsEnabledAtVersion?: SchemaVersion,
+  sendCreatedAsUpdated?: boolean,
+  log?: SyncLog,
+  // Advanced (unsafe) customization point. Useful when you have subtle invariants between multiple
+  // columns and want to have them updated consistently, or to implement partial sync
+  // It's called for every record being updated locally, so be sure that this function is FAST.
+  // If you don't want to change default behavior for a given record, return `resolved` as is
+  // Note that it's safe to mutate `resolved` object, so you can skip copying it for performance.
+  conflictResolver?: SyncConflictResolver,
+  // commits changes in multiple batches, and not one - temporary workaround for memory issue
+  _unsafeBatchPerCollection?: boolean,
 }>
 
 // See Sync docs for usage details
 
 export async function synchronize({
-        database,
-        pullChanges,
-        pushChanges,
-        sendCreatedAsUpdated = false,
-        useSequenceIds = false,
-        migrationsEnabledAtVersion,
-        log,
-        conflictResolver,
-        _unsafeBatchPerCollection,
-      }: SyncArgs): Promise<void> {
+  database,
+  pullChanges,
+  pushChanges,
+  sendCreatedAsUpdated = false,
+  useSequenceIds = false,
+  migrationsEnabledAtVersion,
+  log,
+  conflictResolver,
+  _unsafeBatchPerCollection,
+}: SyncArgs): Promise<void> {
   ensureActionsEnabled(database)
   const resetCount = database._resetCount
   log && (log.startedAt = new Date())
 
   // TODO: Wrap the three computionally intensive phases in `requestIdleCallback`
 
-  // push phase
-  const localChanges = await fetchLocalChanges(database)
-
-  if (!isSameDatabase(database, resetCount))
-    return
-
-  if (!isChangeSetEmpty(localChanges.changes)) {
-    await pushChanges({ changes: localChanges.changes, lastPulledAt: newLastPulledAt })
-
-    if (!isSameDatabase(database, resetCount))
-      return
-
-    await markLocalChangesAsSynced(database, localChanges)
-  }
-
-
   // pull phase
   const lastPulledAt = await getLastPulledAt(database, useSequenceIds)
-
   log && (log.lastPulledAt = lastPulledAt)
 
   const { schemaVersion, migration, shouldSaveSchemaVersion } = await getMigrationInfo(
@@ -148,6 +131,21 @@ export async function synchronize({
       await setLastPulledSchemaVersion(database, schemaVersion)
     }
   }, 'sync-synchronize-apply')
+
+  // push phase
+  const localChanges = await fetchLocalChanges(database)
+
+  if (!isSameDatabase(database, resetCount))
+    return
+
+  if (!isChangeSetEmpty(localChanges.changes)) {
+    await pushChanges({ changes: localChanges.changes, lastPulledAt: newLastPulledAt })
+
+    if (!isSameDatabase(database, resetCount))
+      return
+      
+    await markLocalChangesAsSynced(database, localChanges)
+  }
 
   log && (log.finishedAt = new Date())
 }
