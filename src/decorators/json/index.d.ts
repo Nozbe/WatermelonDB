744b26742e6ab8ec81dcd763f072869a1bfbe0e7
Radek Pietruszewski <radexpl@gmail.com>
2024-08-08 10:03:10 +0200

    reformat

--- src/decorators/json/index.d.ts
+++ src/decorators/json/index.d.ts
@@ -5,9 +5,13 @@ export type Sanitizer = (source: any, model?: Model) => any
 
 export type Options = {
   /** Use cached value if possible rather than sanitizing the raw value for every read. Default: `false` */
-  memo: boolean;
+  memo: boolean
 }
 
-declare function json(rawFieldName: ColumnName, sanitizer: Sanitizer, options?: Options): PropertyDecorator
+declare function json(
+  rawFieldName: ColumnName,
+  sanitizer: Sanitizer,
+  options?: Options,
+): PropertyDecorator
 
 export default json
