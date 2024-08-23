3faf048bf4c59c2b06467b8123133a4dcf8999c5
Jonathan DiCamillo <jonathan@buildops.com>
2024-08-23 15:59:22 -0700

    Revert "chore(sqlite-android): directly added project files instead of aar"
    This reverts commit 702e85c4b7ef1530bd4488a90857d383b24ce4b3.

--- native/android/sqlite-android/src/main/java/io/requery/android/database/sqlite/SQLiteUpdateHook.java
+++
@@ -1,5 +0,0 @@
-package io.requery.android.database.sqlite;
-
-public interface SQLiteUpdateHook {
-    void onUpdateFromNative(int operationType, String databaseName, String tableName, long rowId);
-}
--- native/android/sqlite-android/src/main/jni/Android.mk
+++
@@ -1,4 +0,0 @@
-
-LOCAL_PATH:= $(call my-dir)
-include $(LOCAL_PATH)/sqlite/Android.mk
-
