70ef1dff6b13a2f190abf8f79494c61ab62f85d2
Radek Pietruszewski <radexpl@gmail.com>
2024-11-22 18:04:31 +0100

    fix metro config after rnw dep removal

--- metro.config.js
+++ metro.config.js
@@ -1,13 +1,13 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config')
 const exclusionList = require('metro-config/src/defaults/exclusionList')
-const fs = require('fs')
+// const fs = require('fs')
 const path = require('path')
 const glob = require('glob-to-regexp')
 const metroCache = require('metro-cache')
 
-const rnwPath = fs.realpathSync(
-  path.resolve(require.resolve('react-native-windows/package.json'), '..'),
-)
+// const rnwPath = fs.realpathSync(
+//   path.resolve(require.resolve('react-native-windows/package.json'), '..'),
+// )
 
 const getBlockList = () => {
   const defaultPattern = exclusionList([
@@ -19,9 +19,9 @@ const getBlockList = () => {
     // TODO: Shouldn't it be native/windowsTest?
     new RegExp(`${path.resolve(__dirname, 'windows').replace(/[/\\]/g, '/')}.*`),
     // This prevents "react-native run-windows" from hitting: EBUSY: resource busy or locked, open msbuild.ProjectImports.zip or other files produced by msbuild
-    new RegExp(`${rnwPath}/build/.*`),
-    new RegExp(`${rnwPath}/target/.*`),
-    /.*\.ProjectImports\.zip/,
+    // new RegExp(`${rnwPath}/build/.*`),
+    // new RegExp(`${rnwPath}/target/.*`),
+    // /.*\.ProjectImports\.zip/,
   ])
     .toString()
     .slice(1, -1)
